98cec8297b2b10c358642aa38f82ab0ece934e66
Austin Smith <trunk9slamchest@gmail.com>
2019-09-02 04:46:35 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,104 @@
 def consolidate_cart(cart)
-  # code here
+	key_hash = Hash.new(0)
+
+		con_cart = cart.reduce({}) { |new_hash, sub_hash|
+			sub_hash.each { |key, sub_keys|
+				key_hash[key] += 1
+				sub_keys[:count] = key_hash.fetch(key)
+				new_hash[key] = sub_keys
+			}
+		new_hash
+		}
+
+	return con_cart
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+
+
+def apply_coupons(items, coupons)
+
+	coupon_add = coupons.reduce(items) { |memo, hash|
+
+		if memo.keys.include?(hash[:item])
+		  
+			coupon_keys = hash.shift
+			memo_keys = memo[coupon_keys[1]]
+
+			memo["#{coupon_keys[1]} W/COUPON"] = hash
+
+			hash[:price] = hash.delete :cost
+			hash[:clearance] = memo_keys[:clearance]
+			hash[:count] = hash.delete :num
+		  hash[:price] = hash[:price] / hash[:count]
+
+				if memo_keys[:count] >= hash[:count]
+					memo_keys[:count] = memo_keys[:count] - hash[:count]
+				end
+
+				if coupons.length >= 2 && memo_keys[:count] >= hash[:count]
+					memo_keys[:count] = memo_keys[:count] - hash[:count]
+					hash[:count] += hash[:count]
+				end
+		end
+	memo
+	}
+
+return coupon_add
 end
+
+
 
 def apply_clearance(cart)
-  # code here
+
+	discount_prices = cart.reduce({}) { |memo, sub_hash|
+
+  	food_keys = sub_hash[0]
+  	food_values = sub_hash[1]
+
+  		if food_values[:clearance]
+  			food_values[:price] = food_values[:price] - (food_values[:price] * 0.2).round(2)
+  		end
+
+	  memo[food_keys] = food_values
+
+	memo
+	}
+
+return discount_prices
 end
 
+
+
 def checkout(cart, coupons)
-  # code here
+
+total = 0
+
+consol = consolidate_cart(cart)
+
+coupon = apply_coupons(consol, coupons).reduce({})  { |x, y|
+
+if y[1][:count] >= 1
+	y[1][:price] = y[1][:price] * y[1][:count]
+end
+	x[y[0]] = y[1]
+x
+}
+
+sale = apply_clearance(coupon).reduce({}) { |x, y|
+if y[1][:count] > 0
+x[y[0]] = y[1]
+end
+x
+}
+
+total = sale.reduce(0) { |x, y|
+x += y[1][:price]
+
+	if x >= 100.0
+		x = x - (x * 0.1).round(2)
+	end
+	x
+}
+
+return total
 end
